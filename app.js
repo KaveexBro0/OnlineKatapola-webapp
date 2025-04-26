149bbc24021e1100d809bee151f84504292ef5cf
KaveexBro0 <ai.historical.2006@gmail.com>
2025-04-26 21:11:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+function navigate(page) {
+  switch(page) {
+    case 'account':
+      window.location.href = 'account.html';
+      break;
+    case 'stats':
+      window.location.href = 'stats.html'; // You can create this page later
+      break;
+    case 'rewards':
+      window.location.href = 'rewards.html'; // You can create this page later
+      break;
+    case 'home':
+      window.location.href = 'index.html';
+      break;
+    default:
+      alert('Coming Soon!');
+  }
+}
 window.onload = function() {
   if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.ready();
