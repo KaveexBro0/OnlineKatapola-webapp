1cb73f35b87e8e954ce64871c6d856f6df8572a1
KaveexBro0 <ai.historical.2006@gmail.com>
2025-04-26 21:00:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-function playDice() {
-  alert('🎲 Dice roll coming soon!');
-}
-
-function withdraw() {
-  alert('💸 Withdrawal feature coming soon!');
-}
-
 function navigate(page) {
-  alert('Navigating to: ' + page);
+  switch(page) {
+    case 'account':
+      window.location.href = 'account.html';
+      break;
+    case 'stats':
+      window.location.href = 'stats.html'; // You can create this page later
+      break;
+    case 'rewards':
+      window.location.href = 'rewards.html'; // You can create this page later
+      break;
+    case 'home':
+      window.location.href = 'index.html';
+      break;
+    default:
+      alert('Coming Soon!');
+  }
 }
