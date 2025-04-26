f82f7230d15203c93f17de31e0a10fceed8c7a3d
KaveexBro0 <ai.historical.2006@gmail.com>
2025-04-26 16:48:33 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+# Online Katapola WebApp Starter
+- Upload all files to Netlify / Vercel / GitHub Pages
+- Set WebApp URL in your Telegram Bot using setMenuButton
+- Customize styles, logo, and animations if needed!
---
+++ app.js
@@ -0,0 +1,13 @@
+function playGame() {
+  Telegram.WebApp.showAlert('Redirecting to Play...');
+  Telegram.WebApp.close();
+}
+function withdraw() {
+  Telegram.WebApp.showAlert('Redirecting to Withdraw...');
+  Telegram.WebApp.close();
+}
+function account() {
+  Telegram.WebApp.showAlert('Redirecting to Account Info...');
+  Telegram.WebApp.close();
+}
+Telegram.WebApp.ready();
