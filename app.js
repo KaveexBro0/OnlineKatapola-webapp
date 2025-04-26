b1939f35aa598db5b4c19876999a35364f8eefc7
KaveexBro0 <ai.historical.2006@gmail.com>
2025-04-26 17:26:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function playDice() {
+  alert('🎲 Dice roll coming soon!');
+}
+
+function withdraw() {
+  alert('💸 Withdrawal feature coming soon!');
+}
+
+function navigate(page) {
+  alert('Navigating to: ' + page);
+}
